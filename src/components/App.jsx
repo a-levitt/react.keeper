49d8f4e0eec1d8ed1940080e2fb5dcf7f565cf3f
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-22 18:52:51 +0200

    Implement create notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,27 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+    const [notes, setNotes] = React.useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+          return [...prevNotes, newNote];
+        });
+    }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea
+        onAdd={addNote}
+      />
+        {notes.map((noteItem) => {
+            return <Note
+                title={noteItem.title}
+                content={noteItem.content}
+            />
+        })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [note, setNote] = React.useState({
+      title: "",
+      content: ""
+  });
+
+  function handleChange(event) {
+      const {name, value} = event.target;
+
+      setNote(prevNote => {
+          return {
+              ...prevNote,
+              [name]: value
+          }
+      })
+  }
+
+  function submitNote(event) {
+      props.onAdd(note);
+      event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={note.title}
+        />
+        <textarea
+            name="content"
+            placeholder="Take a note..."
+            rows="3"
+            onChange={handleChange}
+            value={note.content}
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
